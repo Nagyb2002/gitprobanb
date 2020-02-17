5625cfc898c3058fde29b8ff1512cab5d731bde2
Nagy Bálint <gilimbhun02@gmail.com>
2020-02-17 09:35:43 +0100

    autofent

--- gitprobanb/Program.cs
+++ gitprobanb/Program.cs
@@ -8,10 +8,14 @@ namespace gitprobanb
 {
     class Program
     {
-        private
         static void Main(string[] args)
         {
-            
+            auto e1 = new auto("Ferrari", 2012, 100, "benzin", 12000000);
+            Console.WriteLine(e1.ToString());
+            auto e2 = new auto("Audi", 2010, 10000, "disel", 100000);
+            Console.WriteLine(e1.ToString());
+            Console.WriteLine(e2.ToString());
+            Console.ReadKey();
         }
     }
 }
---
+++ gitprobanb/auto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitprobanb
+{
+    class auto
+    {
+        private string modell;
+        private int evj;
+        private int kilom;
+        private string uzema;
+        private int ar;
+
+        public auto(string modell, int evj, int kilom, string uzema, int ar)
+        {
+            this.modell = modell;
+            this.evj = evj;
+            this.kilom = kilom;
+            this.uzema = uzema;
+            this.ar = ar;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4}", modell, evj, kilom, uzema, ar);
+        }
+    }
+}
